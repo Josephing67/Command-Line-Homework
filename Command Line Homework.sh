45d4b834e7443423a9e7c243e784404cbf20ffc7
Josephing67 <80915563+Josephing67@users.noreply.github.com>
2021-05-07 10:50:11 -0600

    Add files via upload

---
+++ Command Line Homework.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+mkdir my-favorite-things
+cd my-favorite-things
+mkdir books
+ls
+cd books
+mkdir haruki-murakami
+ls
+cd haruki-murakami 
+echo "In a Tokyo suburb, a young man named Toru Okada searches for his wife’s missing cat—and then for his wife as well—in a netherworld beneath the city’s placid surface"> the-wind-up-bird-chronicle.txt
+ls
+cd ..
+cd ..
+ls
+#fill in Nano code here --Text-- In a Tokyo suburb, a young man named Toru Okada searches for his wife’s missing cat—and then for his wife as well—in a netherworld beneath the city’s placid surface.#
+mkdir movies
+cd movies
+mkdir Anna-Karina
+ls
+cd Anna-Karina
+echo "A French striptease artist is desperate to become a mother. When her reluctant boyfriend suggests that his best friend impregnate her, feelings become complicated when she accepts"> a-woman-is-a-woman.txt
+#fill in Nano code here --Text-- A French striptease artist is desperate to become a mother. When her reluctant boyfriend suggests that his best friend impregnate her, feelings become complicated when she accepts#
+cd ..
+ls
+touch angela.txt
+ls
+mkdir breakthrough-movie
+mv angela.txt breakthrough-movie
+ls
+cd ..
+mkdir music
+ls
+cd music
+mkdir disco
+ls
+cd disco
+touch ymca.txt
+ls
+cd ..
+ls
+rm -r disco
+ls
+mkdir beatles
+ls
+mkdir beach-boys 
+ls
+mkdir jackson-5
+ls
+cd beatles
+touch yesterday.txt
+ls
+cp yesterday.txt ~/justice_reskill/my-favorite-things/music/beach-boys
+cp yesterday.txt ~/justice_reskill/my-favorite-things/music/jackson-5
+cd ~/justice_reskill/my-favorite-things/music/beach-boys
+ls
+mv yesterday.txt surfinusa.txt
+ls
+cd ~/justice_reskill/my-favorite-things/music/jackson-5
+ls
+mv yesterday.txt abc.txt
+ls
+cd ~/justice_reskill/my-favorite-things
+ls
+mkdir media
+ls
+mv movies music books ~/justice_reskill/my-favorite-things/media
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
